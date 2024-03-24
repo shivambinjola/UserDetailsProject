13a650d04f0f42c3a308eabcb698912f99c996e5
shivambinjola <156425175+GoBabyGoIt@users.noreply.github.com>
2024-03-24 12:27:48 +0530

    second commit

--- src/components/family-members-form.tsx
+++ src/components/family-members-form.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useForm, useFieldArray } from "react-hook-form";
 import Button from "./ui/button";
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Form = ({ setFamilyDetails, setChangecomp }: any) => {
   const {
@@ -39,62 +44,131 @@ const Form = ({ setFamilyDetails, setChangecomp }: any) => {
   return (
     <form className="flex flex-col space-y-8" onSubmit={handleSubmit(onSubmit)}>
       {fields.map((member, index) => (
-        <div className="">
-          <div className="flex items-center justify-between">
-            <h2 className="text-2xl font-semibold xs:text-lg">
-              Family Member {index + 1}{" "}
-            </h2>
-            {index !== 0 && (
-              <Button
-                className="text-lg bg-red-400 px-5 rounded-lg text-white py-1"
-                type="button"
-                onClick={() => remove(index)}
-              >
-                Remove
-              </Button>
-            )}
-          </div>
-          <div className="space-y-5" key={member.id}>
-            <div className="flex flex-col ">
-              <label>Name</label>
-              <input
-                type="text"
-                className={`outline-none border-[1px] ${
-                  errors?.familyMembers?.[index]?.name
-                    ? "border-[#dc3545]"
-                    : "border-[#CCCCCC]"
-                } rounded-lg w-[90vw] h-[40px] pl-2`}
-                {...register(`familyMembers.${index}.name`, { required: true })}
-                defaultValue={member.name}
-                placeholder="Name"
-              />
-              {errors?.familyMembers?.[index]?.name && (
-                <span className="text-[#dc3545] text-sm mt-1">
-                  This field is required
-                </span>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <div className="flex items-center justify-between w-[85vw]">
+              <h2 className="text-2xl font-semibold xs:text-lg">
+                Family Member {index + 1}{" "}
+              </h2>
+              {index !== 0 && (
+                <Button
+                  className="text-lg bg-red-400 px-5 rounded-lg text-white py-1"
+                  type="button"
+                  onClick={() => remove(index)}
+                >
+                  Remove
+                </Button>
               )}
             </div>
-            <div className="flex flex-col ">
-              <label>Age</label>
-              <input
-                type="text"
-                className={`outline-none border-[1px] ${
-                  errors?.familyMembers?.[index]?.age
-                    ? "border-[#dc3545]"
-                    : "border-[#CCCCCC]"
-                } rounded-lg w-[90vw] h-[40px] pl-2`}
-                {...register(`familyMembers.${index}.age`, { required: true })}
-                defaultValue={member.age}
-                placeholder="Age"
-              />
-              {errors?.familyMembers?.[index]?.age && (
-                <span className="text-[#dc3545] text-sm mt-1">
-                  This field is required
-                </span>
-              )}
+          </AccordionSummary>
+          <AccordionDetails>
+            <div className="space-y-5" key={member.id}>
+              <div className="flex flex-col space-y-1 ">
+                <label>Name</label>
+                <input
+                  type="text"
+                  className={`outline-none border-[1px] ${
+                    errors?.familyMembers?.[index]?.name
+                      ? "border-[#dc3545]"
+                      : "border-[#CCCCCC]"
+                  } rounded-lg w-[90vw] h-[40px] pl-2`}
+                  {...register(`familyMembers.${index}.name`, {
+                    required: true,
+                  })}
+                  defaultValue={member.name}
+                  placeholder="Name"
+                />
+                {errors?.familyMembers?.[index]?.name && (
+                  <span className="text-[#dc3545] text-sm mt-1">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col space-y-1">
+                <label>Age</label>
+                <input
+                  type="text"
+                  className={`outline-none border-[1px] ${
+                    errors?.familyMembers?.[index]?.age
+                      ? "border-[#dc3545]"
+                      : "border-[#CCCCCC]"
+                  } rounded-lg w-[90vw] h-[40px] pl-2`}
+                  {...register(`familyMembers.${index}.age`, {
+                    required: true,
+                  })}
+                  defaultValue={member.age}
+                  placeholder="Age"
+                />
+                {errors?.familyMembers?.[index]?.age && (
+                  <span className="text-[#dc3545] text-sm mt-1">
+                    This field is required
+                  </span>
+                )}
+              </div>
             </div>
-          </div>
-        </div>
+          </AccordionDetails>
+        </Accordion>
+
+        // <div className="">
+        //   <div className="flex items-center justify-between">
+        //     <h2 className="text-2xl font-semibold xs:text-lg">
+        //       Family Member {index + 1}{" "}
+        //     </h2>
+        //     {index !== 0 && (
+        //       <Button
+        //         className="text-lg bg-red-400 px-5 rounded-lg text-white py-1"
+        //         type="button"
+        //         onClick={() => remove(index)}
+        //       >
+        //         Remove
+        //       </Button>
+        //     )}
+        //   </div>
+        //   <div className="space-y-5" key={member.id}>
+        //     <div className="flex flex-col ">
+        //       <label>Name</label>
+        //       <input
+        //         type="text"
+        //         className={`outline-none border-[1px] ${
+        //           errors?.familyMembers?.[index]?.name
+        //             ? "border-[#dc3545]"
+        //             : "border-[#CCCCCC]"
+        //         } rounded-lg w-[90vw] h-[40px] pl-2`}
+        //         {...register(`familyMembers.${index}.name`, { required: true })}
+        //         defaultValue={member.name}
+        //         placeholder="Name"
+        //       />
+        //       {errors?.familyMembers?.[index]?.name && (
+        //         <span className="text-[#dc3545] text-sm mt-1">
+        //           This field is required
+        //         </span>
+        //       )}
+        //     </div>
+        //     <div className="flex flex-col ">
+        //       <label>Age</label>
+        //       <input
+        //         type="text"
+        //         className={`outline-none border-[1px] ${
+        //           errors?.familyMembers?.[index]?.age
+        //             ? "border-[#dc3545]"
+        //             : "border-[#CCCCCC]"
+        //         } rounded-lg w-[90vw] h-[40px] pl-2`}
+        //         {...register(`familyMembers.${index}.age`, { required: true })}
+        //         defaultValue={member.age}
+        //         placeholder="Age"
+        //       />
+        //       {errors?.familyMembers?.[index]?.age && (
+        //         <span className="text-[#dc3545] text-sm mt-1">
+        //           This field is required
+        //         </span>
+        //       )}
+        //     </div>
+        //   </div>
+        // </div>
       ))}
       <Button
         className="bg-gray-200 rounded-lg w-full px-20 py-2 font-medium text-lg"
